d5d63e69a2a863f4bd42bff569dea696f6ddd26d
Estera <estera.maftei94@gmail.com>
2021-06-01 13:24:02 +0100

    database

---
+++ collector_2021-06-01.sql
@@ -0,0 +1,60 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 5446
+#
+# https://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.34)
+# Database: collector
+# Generation Time: 2021-06-01 10:36:28 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+SET NAMES utf8mb4;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table cars
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `cars`;
+
+CREATE TABLE `cars` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `make` varchar(255) DEFAULT NULL,
+  `model` varchar(255) DEFAULT NULL,
+  `fuel_type` varchar(255) DEFAULT NULL,
+  `gearbox` varchar(255) DEFAULT NULL,
+  `year` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `cars` WRITE;
+/*!40000 ALTER TABLE `cars` DISABLE KEYS */;
+
+INSERT INTO `cars` (`id`, `make`, `model`, `fuel_type`, `gearbox`, `year`)
+VALUES
+	(1,'Audi','A3','diesel','automatic',2015),
+	(2,'BMW','1 Series','petrol','manual',2011),
+	(3,'Renault','Megane','diesel','manual',2010),
+	(4,'Lamborghini','Aventador','petrol','Automatic',2017),
+	(8,'mazda','1020','Diesel','manual',2018);
+
+/*!40000 ALTER TABLE `cars` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
