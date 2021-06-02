3fea3cd6f1475b521988aec06a2254e6e7e5045c
Estera <estera.maftei94@gmail.com>
2021-06-02 10:19:38 +0100

    story1

--- functions.php
+++ functions.php
@@ -4,11 +4,10 @@ function connectToDB()
 {
     $db = new PDO('mysql:host=db;dbname=collector', 'root', 'password');
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $db;
 }
 
-function getCarsDb()
+function getCarsDb(): array
 {
     $db = connectToDB();
     $query = $db->prepare('SELECT `make`,`model`,`fuel`,`gearbox`,`year` FROM `cars` JOIN `fuel` on `cars` . `fuel_type` = `fuel` . `id`');
@@ -17,21 +16,20 @@ function getCarsDb()
     return $cars;
 }
 
-function displayCars($getCars)
+function displayCars(array $getCars): string
 {
     $display =  '<div class="section">';
     foreach ($getCars as $cars) {
          $display .= '<div class="subSection">'
-                        .'<p class="description">'. 'Make: ' . '<span>'. $cars['make'] .'</span>' . '</p>'
-                        .'<p class="description">'. 'Model: ' . '<span>'. $cars['model'] .'</span>' . '</p>'
-                        .'<p class="description">'. 'Fuel Type: ' . '<span>'. $cars['fuel'] .'</span>' .'</p>'
-                        .'<p class="description">'. 'Gearbox: ' . '<span>'. $cars['gearbox'] .'</span>' . '</p>'
-                        .'<p class="description">'. 'Year: ' . '<span>'. $cars['year'] .'</span>' . '</p>'
+                        .'<p>'. 'Make: ' . '<span>'. $cars['make'] .'</span>' . '</p>'
+                        .'<p>'. 'Model: ' . '<span>'. $cars['model'] .'</span>' . '</p>'
+                        .'<p>'. 'Fuel Type: ' . '<span>'. $cars['fuel'] .'</span>' .'</p>'
+                        .'<p>'. 'Gearbox: ' . '<span>'. $cars['gearbox'] .'</span>' . '</p>'
+                        .'<p>'. 'Year: ' . '<span>'. $cars['year'] .'</span>' . '</p>'
                    . '</div>';
     }
-         $display .= '</div';
+         $display .= '</div>';
 
          return $display;
 }
-
 
