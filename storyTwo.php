8fd7b9d61fc44226283914214f007e614d7aabbe
Estera <estera.maftei94@gmail.com>
2021-06-03 08:50:36 +0100

    validation file added

--- storyTwo.php
+++ storyTwo.php
@@ -1,100 +1,5 @@
 <?php
-require 'functions.php';
-$db = connectToDB();
-
-$make = $model = $fuel = $gearbox = $year = '';
-
-$errors = ['make'=>'',
-    'model'=>'',
-    'fuel'=>'',
-    'gearbox'=>'',
-    'year'=>''
-];
-
-if(isset($_POST['submit']))
-{
-    if(empty($_POST['make']))
-    {
-        $errors['make'] = 'Please add a make!';
-    } else {
-        $make = $_POST['model'];
-    }
-
-    if (empty($_POST['model'])) {
-        $errors['model'] = 'Please add a model name!';
-    } else {
-        $model = $_POST['model'];
-
-        $query = $db->prepare('SELECT `model` FROM `cars` WHERE `model` = ?');
-
-        $query->execute([$model]);
-
-        $modelDb = $query->fetchAll();
-
-        if(!empty($modelDb)) {
-            $errors['model'] = 'This model is already in the list!';
-        } else {
-            $model = $_POST['model'];
-        }
-    }
-
-    if(empty($_POST['fuel']))
-    {
-        $errors['fuel'] = 'Please add a fuel type!';
-    } else {
-        $fuel = $_POST['fuel'];
-    }
-
-    if(empty($_POST['gearbox']))
-    {
-        $errors['gearbox'] = 'Please add gearbox!';
-    } else {
-        $gearbox = $_POST['gearbox'];
-    }
-
-    if(empty($_POST['year']))
-    {
-        $errors['year'] = 'Please add a year!';
-    } else {
-//        var_dump(is_numeric($_POST['year']));
-//        exit;
-        if(!is_numeric($_POST['year'])){
-            $errors['year'] = 'Please add a year as a number!';
-        } else {
-            $year = $_POST['year'];
-        }
-    }
-
-    if (
-            empty($errors['make']) &&
-            empty($errors['model']) &&
-            empty($errors['fuel']) &&
-            empty($errors['gearbox']) &&
-            empty($errors['year'])
-        ) {
-        $make = $_POST['make'];
-
-        $model= $_POST['model'];
-
-        $fuel = $_POST['fuel'];
-
-        $query = $db->prepare('SELECT `id` FROM `fuel` WHERE `fuel` = ?');
-
-        $query->execute([$fuel]);
-
-        $fuelDb = $query->fetchAll();
-
-        $gearbox= $_POST['gearbox'];
-
-        $year= $_POST['year'];
-
-        $query = $db->prepare('INSERT INTO `cars`(`make`, `model`, `fuel_type`, `gearbox`, `year`)
-                                     VALUES (?, ?, ?, ?, ?)');
-        $result = $query->execute([$make, $model, $fuelDb[0]['id'], $gearbox, $year]);
-        header('Location: carsCollection.php');
-        exit;
-    }
-}
+require 'validation.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -125,11 +30,12 @@ if(isset($_POST['submit']))
         <input type="text" name="model" value="<?php echo htmlspecialchars($model) ?>"><br>
         <div class="text"><?php echo $errors['model']; ?></div>
         <label>Fuel Type: </label><br>
-        <select name="fuel">
-            <option>Select fuel type</option>
+        <select name="fuel" value="<?php echo htmlspecialchars($fuel) ?>">
+            <option></option>
             <option>Diesel</option>
             <option>Petrol</option>
         </select><br>
+        <div class="text"><?php echo $errors['fuel']; ?></div>
         <label>Gearbox: </label><br>
         <input type="text" name="gearbox" value="<?php echo htmlspecialchars($gearbox) ?>"><br>
         <div class="text"><?php echo $errors['gearbox']; ?></div>
